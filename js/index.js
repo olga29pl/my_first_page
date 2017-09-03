ff55d37b939585133b33cf85b5fa838214eee446
Olga Plekan <pl.olga29@gmail.com>
2017-09-03 16:22:19 +0000

    Fixed line

--- js/index.js
+++ js/index.js
@@ -1,18 +1,40 @@
-$(function(){
-  $('input[type=submit').click(sendData);
+$(function() {
+  $('#button').click(sendData);
 })
 
 function sendData(e) {
   e.preventDefault();
-  
+
   $.ajax({
-    url: "https://formspree.io/pl.olga29@gmail.com", 
+    url: "https://formspree.io/pl.olga29@gmail.com",
     method: "POST",
+    data: $(this).serialize(),
     dataType: "json",
-    data: $(this).serialize()
-  }).done(function(){
+  }).done(function() {
     $('form')[0].reset();
   })
 }
 
-    
+$(function() {
+    $('a[href="#gallery"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 2000);
+        }
+    });
+});
+
+$(function() {
+    $('a[href="#footer"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 3000);
+        }
+    });
+});
